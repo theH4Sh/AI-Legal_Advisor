233d8f80afb392e2803b72f1ef56e7fbda2d1d1b
Haseeb Shah <syedhaseebnaqvi51214@gmail.com>
2025-07-16 21:02:28 +0500

    width adjusted

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar ({ user }) {
 
 	return (
-		<div className="flex justify-between items-center border-b border-gray-300 p-3">
+		<div className="flex justify-between items-center border-b border-gray-300 p-3 w-full">
 			<div className="font-semibold text-blue-600">AI Legal Advisor</div>
 			<div className="flex justify-center items-center">
 				<div className="text-sm">{user.username}</div>

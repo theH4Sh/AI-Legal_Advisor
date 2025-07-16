f3eda06087b05e26c95fdc0ed80f2edf4e67f107
Haseeb Shah <syedhaseebnaqvi51214@gmail.com>
2025-07-16 19:10:57 +0500

    Navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar ({ user }) {
+
+	return (
+		<div className="flex justify-between items-center border-b border-gray-300 p-3">
+			<div className="font-semibold text-blue-600">AI Legal Advisor</div>
+			<div className="flex justify-center items-center">
+				<div className="text-sm">{user.username}</div>
+				<div className="bg-gray-300 px-5 py-3 rounded-4xl mx-1 text-lg">{user.username.charAt(0).toUpperCase()}</div>
+			</div>
+		</div>
+	)
+}
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,7 +1,15 @@
+import useFetch from '../hooks/useFetch'
+import Navbar from '../components/Navbar'
 export default function Chat () {
+	const url = import.meta.env.VITE_API + 'auth/users/me/'
+	const { data: user, error, loading } = useFetch(url)
+
+	//if (loading) return <p>Loading</p>
+	if (error) return <p>Error...</p>
+
 	return(
 		<div>
-			Chat
+			{ user && <Navbar user={user} /> }
 		</div>
 	)
 }

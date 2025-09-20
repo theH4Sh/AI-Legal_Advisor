d8e6ec6d63ee9dd9ec78ad28d441bb35745e9778
Haseeb Shah <syedhaseebnaqvi51214@gmail.com>
2025-09-20 23:49:59 +0500

    Delete Chat

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import useFetch from '../hooks/useFetch'
 
 export default function Sidebar () {
+	const token = useSelector(state => state.accessToken)
+
 	const [isOpen, setIsOpen] = useState(true)
 
 	const url = import.meta.env.VITE_API + 'chat/'
@@ -15,6 +18,25 @@ export default function Sidebar () {
 			setChats(data)
 		}
 	}, data)
+
+	const handleDelete = async (chatId) => {
+		try {
+			const res = await fetch(`${url}${chatId}/details/`, {
+				method: "DELETE",
+				headers: {
+					"Authorization": `Bearer ${token}`,
+					"Content-Type": "application/json"
+				}
+			})
+
+			if (res.ok) {
+				//remove chat from state
+				setChats(prev => prev.filter(chat=> chat.id != chatId))
+			}
+		} catch (err) {
+			console.log(err)
+		}
+	}
 
 	if (error) {
 		console.log(error)
@@ -93,7 +115,7 @@ export default function Sidebar () {
 										className="hover:bg-gray-200 px-2 py-2 rounded-lg cursor-pointer flex justify-between place-items-center"
 									>
 										<p>{firstMessage.length > 30? firstMessage.slice(0, 20) + "..." : firstMessage}</p>
-										<button className="text-red-500 cursor-pointer">
+										<button onClick={() => handleDelete(chat.id)} className="text-red-500 cursor-pointer">
 											<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
 											  <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
 											</svg>

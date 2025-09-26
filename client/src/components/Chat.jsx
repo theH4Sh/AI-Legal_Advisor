efc0daeaf774fa5c982673addc51c518b2870fd5
Haseeb Shah <syedhaseebnaqvi51214@gmail.com>
2025-09-26 23:16:59 +0500

    Send Message functionality created

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react'
 import useFetch from '../hooks/useFetch'
+import useSend from '../hooks/useSend'
 
 export default function Chat ({ chatId }) {
 	const [messages, setMessages] = useState([])
 	const url = import.meta.env.VITE_API + `chat/${chatId}/details/`
 	const {data, loading, error} = useFetch(url)
+
 	useEffect(() => {
 		if (data) {
 			setMessages(data.messages)
@@ -14,6 +16,29 @@ export default function Chat ({ chatId }) {
 	if (error) {
 		console.log("Error in message: " + error)
 	}
+
+	const [newPrompt, setNewPrompt] = useState("")
+
+	const { sendMessage } = useSend(import.meta.env.VITE_API + `chat/${chatId}/message/`)
+
+	const handleSubmit = async () => {
+		const formattedNewMsg = {
+			id: Date.now(),
+			sender: "user",
+			content: newPrompt
+		}
+		setMessages(prev => [...prev, formattedNewMsg])
+		setNewPrompt("")
+
+		const res = await sendMessage(newPrompt)
+		if (res) {
+			console.log("response:", res.bot)
+			setMessages(prev => [...prev, res.bot])
+		}
+
+		console.log("updated: ", messages)
+	}
+
 	return (
 		<div className="flex flex-col place-items-center min-h-screen overflow-y-auto
 			pt-25 pb-60">
@@ -21,19 +46,28 @@ export default function Chat ({ chatId }) {
 				{loading && <p className="text-gray-400">Loading...</p>}
 				{messages.length > 0 ? messages.map(msg => (
 					<div key={msg.id} className={`flex my-2 ${ msg.sender == 'user' ? 'justify-end' : 'justify-start'}`}>
-						<div className={`rounded-2xl px-3 py-2 max-w-[70%] ${ msg.sender == 'user' ? 'bg-blue-400 rounded-br-none' : 'bg-slate-200 rounded-bl-none'}`}>
+						<div className={` text-start rounded-2xl px-3 py-2 max-w-[70%] ${ msg.sender == 'user' ? 'bg-blue-400 rounded-br-none' : 'bg-slate-200 rounded-bl-none'}`}>
 							{msg.content}
 						</div>
 					</div>
 					)) : (<div>No chats found</div>)
 				}
-				{/*{example}*/}
 			</div>
-			<div className="fixed bottom-0 my-3 w-[450px] md:w-full md:max-w-3xl bg-gray-200 rounded-4xl">
+			<div className="fixed bottom-0 my-3 w-[450px] md:w-full md:max-w-3xl bg-gray-200 rounded-4xl flex place-items-center">
 				<textarea 
-					rows={3}
+					rows={1}
 					placeholder="Send a message"
-					className="flex-1 resize-none focus:outline-none w-full h-full overflow-hidden p-3" />
+					value={newPrompt}
+					onChange={(e) => setNewPrompt(e.target.value)}
+					className="flex-1 resize-none focus:outline-none w-full h-full overflow-auto p-3" 
+				/>
+				<button 
+					onClick={handleSubmit}
+					className="bg-blue-500 p-2 m-2 rounded-full cursor-pointer hover:bg-blue-400 transition transition-delay">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+					  <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+					</svg>
+				</button>
 			</div>
 		</div>
 	)

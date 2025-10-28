7fae65fbc23c2a0c1b3dee6e77106a41cfffbc6b
Haseeb Shah <syedhaseebnaqvi51214@gmail.com>
2025-10-29 00:24:12 +0500

    Logout functionality

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,12 +2,32 @@ import { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import useFetch from '../hooks/useFetch'
 import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { logout } from '../authSlice'
 
 export default function Sidebar ({ isOpen, setIsOpen }) {
 	const token = useSelector(state => state.accessToken)
 	const url = import.meta.env.VITE_API + 'chat/'
 	const { data, error, loading } = useFetch(url)
 	const [chats, setChats] = useState([])
+
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
+
+	const handleLogout = () => {
+	// Clear Redux state
+	dispatch(logout())
+
+    // Remove token from localStorage
+	localStorage.removeItem('auth')
+
+	// Redirect to login page
+	navigate('/')
+
+	// Close sidebar
+	setIsOpen(true)
+	}
 
 	useEffect(() => {
 		if (data) {
@@ -131,6 +151,19 @@ export default function Sidebar ({ isOpen, setIsOpen }) {
 							}) 
 						}
 					</ul>
+				</div>
+
+				{/*Logout Button*/}
+				<div className="absolute bottom-5 left-0 w-full px-5">
+					<button
+						onClick={handleLogout}
+						className="w-full flex items-center justify-center bg-gray-900 text-white py-2 rounded-md hover:bg-gray-800 cursor-pointer transition"
+					>
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+						  <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+						</svg>
+
+					</button>
 				</div>
 			</div>
 		</>

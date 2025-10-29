e3e20cee8968d398bcc9e56d395d49e3ed4dd09b
Haseeb Shah <syedhaseebnaqvi51214@gmail.com>
2025-10-29 19:26:41 +0500

    Route protection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Navigate } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login'
 import RootLayout from './layout/RootLayout'
@@ -7,17 +7,20 @@ import SignUp from './pages/SignUp'
 import Home from './pages/Home'
 import NewChat from './pages/NewChat'
 import GenerateDoc from './pages/GenerateDoc'
+import { useSelector } from "react-redux"
 
 function App() {
+
+  const auth = useSelector(state => state.isAuthenticated)
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<RootLayout />}>
-        <Route index element={<Login />} />
-        <Route path='signup' element={<SignUp />} />
-        <Route path='chat' element={<NewChat />} />
-        <Route path='chat/:chatId' element={<Home />} />
-        <Route path='generate' element={<GenerateDoc />} />
+        <Route index element={auth ? <Navigate to='/chat' /> : <Login />} />
+        <Route path='signup' element={auth ? <Navigate to='/chat' /> : <SignUp />} />
+        <Route path='chat' element={auth ? <NewChat /> : <Navigate to="/" />} />
+        <Route path='chat/:chatId' element={auth ? <NewChat /> : <Navigate to="/" />} />
+        <Route path='generate' element={auth ? <NewChat /> : <Navigate to="/" />} />
       </Route>
     )
   )

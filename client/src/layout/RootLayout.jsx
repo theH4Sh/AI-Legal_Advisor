187215840840f871016c26eddc2cbe1ad4ecbf25
Haseeb Shah <syedhaseebnaqvi51214@gmail.com>
2025-10-29 19:10:25 +0500

    sidebar and navbar fixed

--- client/src/layout/RootLayout.jsx
+++ client/src/layout/RootLayout.jsx
@@ -2,14 +2,21 @@ import { Outlet } from "react-router-dom";
 import Sidebar from '../components/Sidebar'
 import Navbar from '../components/Navbar'
 import { useState} from "react"
+import { useSelector } from "react-redux";
 
 const RootLayout = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+    const isAuthenticated = useSelector((state) => state.isAuthenticated)
+
     return ( 
         <div>
-            <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
+            {isAuthenticated && (
+                <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
+            )}
             <div>
+            {isAuthenticated && (
                 <Navbar toggleSidebar={() => setIsSidebarOpen(!setIsSidebarOpen)} />
+            )}
                 <Outlet />
             </div>
         </div>
